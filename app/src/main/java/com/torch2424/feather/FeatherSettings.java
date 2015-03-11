25ca8d2e78085f5d24944aab5a4b19a807774673
torch2424 <torch2424@hotmail.com>
2015-03-11 14:54:56 -0700

    Initialized Oncreate to get our toast and background initialized, and commented our menu functions since we are not using them

--- app/src/main/java/com/torch2424/feather/FeatherSettings.java
+++ app/src/main/java/com/torch2424/feather/FeatherSettings.java
@@ -1,21 +1,69 @@
 package com.torch2424.feather;
 
 import android.app.Activity;
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class FeatherSettings extends Activity {
+
+    // for toasts context
+    Toasty toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fsettings);
+
+        // Get our Toast
+        toast = new Toasty(this);
+
+        // Get our wallpaper
+        getAppWallpaper();
+
+        //Do stuff from stats monitor to initialize our settings page and stuff
+    }
+
+    // Getting the system wallpaper and setting it a the apps
+    public void getAppWallpaper()
+    {
+        // Gotten from stack to set background as system background
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
+        Drawable wallpaperDrawable = (Drawable) wallpaperManager.getDrawable();
+
+        //Do cropping of the wallpaper so it doesn't look smushed
+        Bitmap bitmap = ((BitmapDrawable) wallpaperDrawable).getBitmap();
+
+        //Get our display size
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        //crop from center to screensize
+        bitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height);
+
+        //Set wallpaper to bitmap
+        wallpaperDrawable = new BitmapDrawable(getResources(),bitmap);
+
+        // Darken
+        wallpaperDrawable.setColorFilter(
+                getResources().getColor(R.color.transparent),
+                PorterDuff.Mode.SRC_ATOP);
+        getWindow().setBackgroundDrawable(wallpaperDrawable);
     }
 
 
+    /* DOnt need menu stuff, Settings Activity does not need settings
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -34,8 +82,9 @@ public class FeatherSettings extends Activity {
         //noinspection SimplifiableIfStatement
         /*if (id == R.id.action_settings) {
             return true;
-        }*/
+        }/
 
         return super.onOptionsItemSelected(item);
     }
+        */
 }
