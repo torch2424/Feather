86b6899b78cf124e2f29a508d78bab4e10156c33
torch2424 <torch2424@hotmail.com>
2015-03-20 17:50:52 -0700

    Added try catch block to catch exceptions, and some more class optimization

--- app/src/main/java/com/torch2424/feather/SongComparator.java
+++ app/src/main/java/com/torch2424/feather/SongComparator.java
@@ -26,47 +26,61 @@ public class SongComparator implements Comparator<File>
     @Override
     public int compare(File song1, File song2)
     {
-        //Check if they are not music files
-        if(song1 == null || song2 == null || !Manly.isMusic(song1) || !Manly.isMusic(song2))
+        //Place into a try/catch block to catch our exceptions and return 0
+        try {
+            //Check if they are not music files
+            if (!Manly.isMusic(song1) || !Manly.isMusic(song2)) {
+                //return 0 because we cant compare these files
+                return 0;
+            }
+
+            //Since they are music
+
+            //Get our metaData album, song
+
+            //Get the album for song 1 and 2
+            mmr.setDataSource(song1.getAbsolutePath());
+            array1[0] = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+            mmr.setDataSource(song2.getAbsolutePath());
+            array2[0] = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+
+            //Check they are not null
+            if (array1[0] == null || array2[0] == null) return 0;
+
+            //Compare their albums
+            compared = array1[0].compareTo(array2[0]);
+
+            //Our check if we can stop here
+            if (compared < 0) return -10;
+            else if (compared > 0) return 10;
+
+            //Since their albums are equal
+
+            //Get the song number for song 1 and 2
+            mmr.setDataSource(song1.getAbsolutePath());
+            array1[1] = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_CD_TRACK_NUMBER);
+            mmr.setDataSource(song2.getAbsolutePath());
+            array2[1] = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_CD_TRACK_NUMBER);
+
+            //Check they are not null
+            if (array1[1] == null || array2[1] == null) return 0;
+
+            //Compare their songs
+            //First split up the string we get, and return the answer
+            //Get the string of the first half of a cd number (eg. 1/10) and convert it
+            //and integer
+            int trackno1 = Integer.valueOf(array1[1].split("/")[0]);
+            int trackno2 = Integer.valueOf(array2[1].split("/")[0]);
+            //now compare the track numbers to get compared
+            if (trackno1 > trackno2) return 10;
+            else if (trackno1 < trackno2) return -10;
+            else return 0;
+        }
+        catch(Exception e)
         {
-            //return 0 because we cant compare these files
+            //Return 0 if there is an exception
+            Log.d("FeatherException", "Exception thrown:" + e);
             return 0;
         }
-
-        //Since they are music
-
-        //Get our metaData album, song
-
-        //Get the album for song 1 and 2
-        mmr.setDataSource(song1.getAbsolutePath());
-        array1[0] = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
-        mmr.setDataSource(song2.getAbsolutePath());
-        array2[0] = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
-
-        //Compare their albums
-        compared = array1[0].compareTo(array2[0]);
-
-        //Our check if we can stop here
-        if(compared < 0) return -10;
-        else if(compared > 0) return 10;
-
-        //Since their albums are equal
-
-        //Get the song number for song 1 and 2
-        mmr.setDataSource(song1.getAbsolutePath());
-        array1[1] = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_CD_TRACK_NUMBER);
-        mmr.setDataSource(song2.getAbsolutePath());
-        array2[1] = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_CD_TRACK_NUMBER);
-
-        //Compare their songs
-        //First split up the string we get, and return the answer
-        //Get the string of the first half of a cd number (eg. 1/10) and convert it
-        //and integer
-        int trackno1 = Integer.valueOf(array1[1].split("/")[0]);
-        int trackno2 = Integer.valueOf(array2[1].split("/")[0]);
-        //now compare the track numbers to get compared
-        if(trackno1 > trackno2) return 10;
-        else if(trackno1 < trackno2) return -10;
-        else return 0;
     }
 }
